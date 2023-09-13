aadc73c31004728bed14be425842be4b043d1e1b
Himaninamdeo <144206748+Himaninamdeo@users.noreply.github.com>
2023-09-13 12:38:48 +0530

    Create joint.java

---
+++ joint.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[]args){
+        
+        //create first String
+        String first = "java";
+        System.out.println("First String:" + first);
+        
+        //create second 
+        String second = "programming";
+        System.out.println("Second String:" + second);
+        
+        //join two String
+        String joinedString = first.concat(second);
+        System.out.println("Joined String: " + joinedString);
+    }
+}
+
