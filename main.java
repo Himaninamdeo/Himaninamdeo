83d1471fee1d0b221393a27bd8793bf541090fbd
Himaninamdeo <144206748+Himaninamdeo@users.noreply.github.com>
2023-09-13 12:37:33 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+//To check whether the string is empty or TypeNotPresentException
+class Main{
+    public static void main(String args[]){
+        
+        //create a String
+        String s="hello";
+        boolean bs.isEmpty();
+        System.out.println(b);
+        System.out.println(s.isEmpty());
+    }
+}
+
