e1fb932f4a5c7149de13ad89f8ce431c1685d547
Himaninamdeo <144206748+Himaninamdeo@users.noreply.github.com>
2023-09-13 12:36:15 +0530

    Create length.java

---
+++ length.java
@@ -0,0 +1,10 @@
+public class Ma{
+    public static void (String[]args){
+        //create a String
+        String greet ="Hello ! World";
+        System.out.println("String:"+greet);
+        //get the length of greet
+        int length = greet.length();
+        System.out.println("Length:"+length);
+    }
+}
