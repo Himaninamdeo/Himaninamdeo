9c98f4cdb481e5cbfd02fe54edd9de8492a96615
Himaninamdeo <144206748+Himaninamdeo@users.noreply.github.com>
2023-09-13 12:35:36 +0530

    Create ma.java

---
+++ ma.java
@@ -0,0 +1,15 @@
+public class Ma{
+    public static void main(String[]args){
+        //create String
+        String first ="Java";
+        String second ="Python";
+        String third ="JavaScript";
+    
+        //print String
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+    }
+    
+        
+}
