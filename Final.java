948ba93a4af5e1fb7d23c2145fafd27460d24c06
Himaninamdeo <144206748+Himaninamdeo@users.noreply.github.com>
2023-10-11 12:34:33 +0530

    Create Final.java

---
+++ Final.java
@@ -0,0 +1,16 @@
+class FinalDemo{
+    public void display(){
+        System.out.println("This is a final method,");
+    }
+}
+class Main extends FinalDemo{
+    public void display(){
+        System.out.println("The final method is ovverriden");
+    }
+    public static void main(String[] args){
+        Main obj = new Main();
+        obj.display();
+    }
+}
+
+    
