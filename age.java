a6abc4407c0c5ba3b3eade39080beea9d96c721e
Himaninamdeo <144206748+Himaninamdeo@users.noreply.github.com>
2023-10-11 12:29:06 +0530

    Create age.java

---
+++ age.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String[] args){
+        final int AGE = 32;
+        age = 45;
+        System.out.println("Age:"=AGE);
+        }
+}
+Class A extends Main{
+     public statics void main(String[] args){
+     Main m=new Main();
+     m.age=35;
+     }
+}
