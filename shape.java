ad2a5e73881d3ad893550ca7822739e4239659ed
Himaninamdeo <144206748+Himaninamdeo@users.noreply.github.com>
2023-10-11 12:52:49 +0530

    Create shape.java

---
+++ shape.java
@@ -0,0 +1,58 @@
+class Shape {
+    public double area() {
+        return 0.0; 
+    }
+    
+    public double perimeter() {
+        return 0.0; 
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius; 
+    }
+
+    
+    public double perimeter() {
+        return 2 * Math.PI * radius; 
+    }
+}
+
+
+class Square extends Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+  
+    public double area() {
+        return side * side; 
+    }
+
+  
+    public double perimeter() {
+        return 4 * side; 
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Square square = new Square(4.0);
+
+        System.out.println("Circle Area: " + circle.area());
+        System.out.println("Circle Perimeter: " + circle.perimeter());
+
+        System.out.println("Square Area: " + square.area());
+        System.out.println("Square Perimeter: " + square.perimeter());
+    }
+}
